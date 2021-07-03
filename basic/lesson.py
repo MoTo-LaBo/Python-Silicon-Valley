507fe6bc126ee8c9e3d79891bc13d03f56c6666b
MoTo <moto.labo.desgin@gmail.com>
2021-07-03 23:28:17 +0900

    update

--- basic/lesson.py
+++ basic/lesson.py
@@ -1,7 +1,84 @@
+import math
+
 # 変数宣言
 num = 1
+name = 'Mike'
+is_ok = True
+
+print(num, type(num))
+print(name, type(name))
+print(is_ok, type(is_ok))
+
+# 型変換
+num = name
+print(num, type(num))
+
 name = '1'
+new_name = int(name)
+print(new_name, type(new_name))
 
-new_num = int(name)
-print(new_num, type(new_num))
+# print
+print('hi', 'Mike')
+print('hi', 'Mike', sep=',')  # sep -> カンマで区切ることができる
+print('hi', 'Mike', sep=',', end='.\n')  # \n 改行
 
+# 変数
+print(2 + 2)
+
+# math を import すれば math の method で数学関数を使用する事ができる
+result = math.sqrt(25)
+print(result)
+
+y = math.log2(10)
+print(y)
+# print(help(math))
+
+# 文字列
+s = 'test'
+print(s)
+print('hello')
+print('I don\'t know')
+print('say"I don\'t know"')
+print("say\"I don't know\"")
+
+print('hello. How are you?')
+print('hello.\nHow are you?')
+print('C:\name\name')
+print(r'C:\name\name')
+
+print('################')
+print("""
+line1
+line2
+line3
+""")
+print('################')
+
+
+print('################')
+print("""line1
+line2
+line3""")
+print('################')
+print('################')
+print("""\
+line1
+line2
+line3\
+""")
+print('################')
+
+# ''(シングルコーテートで囲うモノ) = リテラル
+print('Hi.' * 3 + 'Mike')
+print('py' + 'thon')
+prefix = 'py'
+print(prefix + 'thon')
+
+# 一行で記述しているのと同じ
+s = ('aaaaaaaaaaaaaaaaa'
+     'bbbbbbbbbbbbbbbbb')
+print(s)
+# \ (バックスラッシュ)でも同じ記述はできるが… 見易さから考えると()が良い
+s = 'aaaaaaaaaaaaaaaaa'\
+     'bbbbbbbbbbbbbbbbb'
+print(s)
